da977703ce1eeafdf5e0f80eef760ef0bca399f2
Xu Mengxiang <834489218@qq.com>
2017-06-20 23:51:58 +0800

    Response different prompt if no data

--- src/service/ListTango.java
+++ src/service/ListTango.java
@@ -36,78 +36,66 @@ public class ListTango extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");   //设置文本形式和字符编码
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html"); // 设置文本形式和字符编码
 		response.setCharacterEncoding("UTF-8");
-		Writer out=response.getWriter();  
+		Writer out = response.getWriter();
+		JSONObject res = new JSONObject();
 
 		try {
-			JSONObject res = new JSONObject();
-			res.put(Constants.RESPONSE_STATUS, Constants.STATUS_QUERY_SUCCESS);
-			res.put(Constants.RESPONSE_PROMPT, "获取成功");
-			JSONArray entityList = new JSONArray();
-			
-			List<Tango> tangoList = new ArrayList<>();
 			JDBCUtil db = JDBCUtil.getInstance();
 			db.connectDB();
-			ResultSet rs = db.executeQuery("SELECT id, writing, pronunciation, tone, meaning, part_of_speech"
-						+ " FROM tango",
-						new Object[]{});
+			ResultSet rs = db.executeQuery(
+					"SELECT id, writing, pronunciation, tone, meaning, part_of_speech" + " FROM tango",
+					new Object[] {});
 
 			if (rs != null) {
-				while (rs.next()) {
-					Tango t = new Tango();
-					t.id = rs.getInt("id");
-					t.writing = rs.getString("writing");
-					t.pronunciation = rs.getString("pronunciation");
-					t.tone = rs.getInt("tone");
-					t.meaning = rs.getString("meaning");
-					t.partOfSpeech = rs.getString("part_of_speech");
-					tangoList.add(t);
+				if (rs.next()) {
+					res.put(Constants.RESPONSE_STATUS, Constants.STATUS_QUERY_SUCCESS);
+					res.put(Constants.RESPONSE_PROMPT, "获取成功");
+					JSONArray entityList = new JSONArray();
+					List<Tango> tangoList = new ArrayList<>();
+					do {
+						Tango t = new Tango();
+						t.id = rs.getInt("id");
+						t.writing = rs.getString("writing");
+						t.pronunciation = rs.getString("pronunciation");
+						t.tone = rs.getInt("tone");
+						t.meaning = rs.getString("meaning");
+						t.partOfSpeech = rs.getString("part_of_speech");
+						tangoList.add(t);
+					} while (rs.next());
+
+					for (Tango t : tangoList) {
+						String strings[] = new String[] { t.writing, // 0
+								t.pronunciation, // 1
+								t.meaning, // 2
+								String.valueOf(t.tone), // 3
+								t.partOfSpeech, // 4
+								t.image, // 5
+								t.voice, // 6
+								String.valueOf(t.score), // 7
+								String.valueOf(t.frequency), // 8
+								String.valueOf(t.addTime.getTime()), // 9
+								String.valueOf(t.lastTime.getTime()), // 10
+								t.flags, // 11
+								String.valueOf(t.delFlag), // 12
+								t.type // 13
+						};
+						String item = StrUtil.join(strings, ",");
+						entityList.put(item);
+					}
+					res.put(Constants.RESPONSE_ENTITIES, entityList);
+				} else {
+					res.put(Constants.RESPONSE_STATUS, Constants.STATUS_EXECUTE_SUCCESS);
+					res.put(Constants.RESPONSE_PROMPT, "暂无数据");
 				}
 			}
-			
-//			Tango t1 = new Tango();
-//			t1.id = -2;
-//			t1.writing = "大切";
-//			t1.pronunciation = "たいせつ";
-//			t1.tone = 0;
-//			t1.meaning = "重要,珍贵";
-//			t1.partOfSpeech = "形容动词";
-//			tangoList.add(t1);
-//			
-//			Tango t2 = new Tango();
-//	        t2.id = -3;
-//	        t2.writing = "ありがとうございます";
-//	        t2.pronunciation = "ありがとうございます";
-//	        t2.meaning = "谢谢";
-//	        t2.partOfSpeech = "惯用语";
-//	        tangoList.add(t2);
-			
-			for (Tango t : tangoList) {
-				String strings[] = new String[]{
-	                    t.writing, //0
-	                    t.pronunciation, //1
-	                    t.meaning, //2
-	                    String.valueOf(t.tone), //3
-	                    t.partOfSpeech, //4
-	                    t.image, //5
-	                    t.voice, // 6
-	                    String.valueOf(t.score), //7
-	                    String.valueOf(t.frequency), //8
-	                    String.valueOf(t.addTime.getTime()), //9
-	                    String.valueOf(t.lastTime.getTime()), //10
-	                    t.flags, //11
-	                    String.valueOf(t.delFlag), //12
-	                    t.type //13
-	            };
-				String item = StrUtil.join(strings, ",");
-				entityList.put(item);
-			}
-			
-			res.put(Constants.RESPONSE_ENTITIES, entityList);
+
 			out.append(res.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
